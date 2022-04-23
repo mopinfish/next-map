57b3670e7fdd94d1c3038668e6494e3f8dceae50
OtsukaNoboru <mopinfish@gmail.com>
2022-04-23 14:45:45 +0900

    とりあえずOLでOSM地図を表示

--- pages/map.tsx
+++ pages/map.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 //import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import MapPane from '../components/MapPane';
+import styles from '../styles/Map.module.css'
 
 export default function Map() {
   return (
@@ -15,6 +16,8 @@ export default function Map() {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+
+        <MapPane />
       </main>
 
       <footer className={styles.footer}>
