99f1ed57330c4abcc3036d076b7a223e52a02b5b
OtsukaNoboru <mopinfish@gmail.com>
2022-04-29 20:51:42 +0900

    関数コンポーネントへの書き換え

--- src/components/MapPane.tsx
+++ src/components/MapPane.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useEffect, useRef } from "react";
 // OpenLayers読み込み
 import Map from "ol/Map";
 import View from "ol/View";
@@ -7,35 +7,40 @@ import OlSourceOSM from "ol/source/OSM";
 import { fromLonLat } from "ol/proj";
 import "ol/ol.css";
 
-class MapPane extends Component {
-  map: any;
-  container: any;
+// 何故かMapPaneが2回マウントされるため、地図が作成されたかを管理するための変数を導入している。解消されたら下記は削除する。
+let map: Map|undefined = undefined
 
-  componentDidMount() {
-    // マップ設定
-    this.map = new Map({
-      target: this.container,
-      layers: [
-        new TileLayer({
-          source: new OlSourceOSM(),
-        }),
-      ],
-      view: new View({
-        center: fromLonLat([139.767, 35.681]),
-        zoom: 14,
-      }),
-    });
-  }
+const MapPane = () => {
+  const containerRef = useRef(null)
 
-  render() {
-    return (
-      <div
-        className={"map"}
-        ref={(e) => (this.container = e)}
-        style={{ minWidth: "800px", minHeight: "800px" }}
-      />
-    );
-  }
+
+  useEffect(() => {
+    const target = containerRef.current 
+
+    if (target != null && map == undefined) {
+      // マップ設定
+      map = new Map({
+        target: target,
+        layers: [
+          new TileLayer({
+            source: new OlSourceOSM(),
+          }),
+        ],
+        view: new View({
+          center: fromLonLat([139.767, 35.681]),
+          zoom: 14,
+        })
+      })
+    }
+  }, [containerRef])
+
+  return (
+    <div
+      className={"map"}
+      ref={containerRef}
+      style={{ minWidth: "800px", minHeight: "800px" }}
+    />
+  )
 }
 
 export default MapPane;
---
+++ src/pages/map.tsx
@@ -0,0 +1,43 @@
+import Head from 'next/head'
+//import Image from 'next/image'
+import MapPane from '../components/MapPane';
+
+const styles = {
+  container: {
+    padding: '0 2rem'
+  },
+  main: {
+    minHeight: '100vh',
+    padding: '4rem 0',
+    flex: 1,
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  title: {
+    textAlign: 'center'
+  }
+}
+
+export default function Map() {
+  return (
+
+    <div style={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <meta name="description" content="map page" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main style={styles.main}>
+        <h1 style={styles.title}>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+
+        <MapPane />
+      </main>
+
+    </div>
+  )
+}
